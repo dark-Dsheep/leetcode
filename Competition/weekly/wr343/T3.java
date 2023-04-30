b0df5f749c9b95fdf55e047f1a6ca721edc4a38b
wawawa <1457480465@qq.com>
2023-04-30 14:46:36 +0800

    LC_Weekly343 2/4

--- Competition/weekly/wr343/T3.java
+++ Competition/weekly/wr343/T3.java
@@ -1,10 +1,15 @@
 package Competition.weekly.wr343;
 
+import java.util.PriorityQueue;
+
 public class T3 {
 
-    public int minimumCost(int[] start, int[] target, int[][] specialRoads) {
-
+    public int minimumCost(int[] start, int[] target, int[][] spe) {
 
         return -1;
+    }
+
+    int calc(int x1, int y1, int x2, int y2) {
+        return Math.abs(x2 - x1) + Math.abs(y2 - y1);
     }
 }
--- Competition/weekly/wr343/T4.java
+++ Competition/weekly/wr343/T4.java
@@ -4,8 +4,21 @@ public class T4 {
 
 
     public String smallestBeautifulString(String s, int k) {
-
-
-        return "";
+        char[] c = s.toCharArray();
+        c[s.length() - 1]++;
+        for (int i = s.length() - 1; i < s.length(); ) {
+            if (c[i] == 'a' + k) {
+                c[i--] = 'a';
+                if (i < 0) {
+                    return "";
+                }
+                c[i]++;
+            } else if (i > 0 && c[i] == c[i-1] || i > 1 && c[i] == c[i-2]) {
+                c[i]++;
+            } else {
+                i++;
+            }
+        }
+        return String.valueOf(c);
     }
 }
