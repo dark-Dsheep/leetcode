76b85cfe98a87f4e7f8f9892cf04d943f5c1bb3e
wawawa <1457480465@qq.com>
2023-05-13 13:48:44 +0800

    2023/5/13 前缀和技巧 LC1685. 有序数组中差绝对值之和

--- 技巧/前缀和/LC_1685.java
+++ 技巧/前缀和/LC_1685.java
@@ -1,33 +1,34 @@
 package 技巧.前缀和;
 
+import java.util.Arrays;
+
 public class LC_1685 {
 
     /*
         1685. 有序数组中差绝对值之和
-        TODO
      */
 
     public int[] getSumAbsoluteDifferences(int[] nums) {
         int n = nums.length;
-        int[] preSum = new int[n];
-        int[] sufSum = new int[n];
-        preSum[0] = 0;
-        sufSum[n - 1] = 0;
-        for (int i = 1; i < n; i++) {
-            preSum[i] = preSum[i - 1] + Math.abs(preSum[i - 1] - nums[i]);
-        }
-        for (int i = n - 2; i >= 0; i--) {
-            sufSum[i] = sufSum[i + 1] + Math.abs(sufSum[i + 1] - nums[i]);
-        }
-        int[] res = new int[n];
+        var sum = new int[n+1]; // 前缀和数组
         for (int i = 0; i < n; i++) {
-            res[i] = preSum[i] + sufSum[i];
+            sum[i+1] = sum[i] + nums[i];
         }
-        return res;
+        var ans = new int[n];
+        // ans[i]表示nums[i]与数组其他元素的差值之和
+        // 等价于以i为分界点, 求Sum(i左边元素的元素差值之和)+Sum(i右边元素的元素差值之和)
+        // left = i左边的前缀和 - 需要减去i个nums[i]
+        // right = i右边的前缀和 - 需要减去n-i个nums[i]
+        for (int i = 0; i < n; i++) {
+            int left = Math.abs(sum[i] - i * nums[i]);
+            int right = Math.abs(sum[n] - sum[i] - (n-i) * nums[i]);
+            ans[i] = left + right;
+        }
+        return ans;
     }
 
     public static void main(String[] args) {
         int[] nums = {1, 4, 6, 8, 10};
-        System.out.println(new LC_1685().getSumAbsoluteDifferences(nums));
+        System.out.println(Arrays.toString(new LC_1685().getSumAbsoluteDifferences(nums)));
     }
 }
