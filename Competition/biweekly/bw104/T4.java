d30dea643f8e19de52b99a43238d7d2114317a40
wawawa <1457480465@qq.com>
2023-05-14 15:34:24 +0800

    2023/5/14 LC104双周赛 补题T4

--- Competition/biweekly/bw104/T4.java
+++ Competition/biweekly/bw104/T4.java
@@ -1,12 +1,22 @@
 package Competition.biweekly.bw104;
 
+import java.util.Arrays;
+
 public class T4 {
 
+    static final long MOD = 1000000007;
 
     public int sumOfPower(int[] nums) {
-
-
-        return -1;
+        Arrays.sort(nums);
+        // 枚举x = nums[i]作为最大值
+        // x一个数的力量 => x^3 -> mx^2 * mn^1
+        // a b c 枚举到c时, a作为最小值, b, c可选可不选 -> 一共2^2种方案 => a的贡献为 a * 2^2
+        long ans = 0, left = 0; // left 记作左侧元素对答案的贡献
+        for (long x : nums) {
+            ans = (ans + x * x % MOD * (x + left)) % MOD;
+            left = (left * 2 + x) % MOD;
+        }
+        return (int) ans;
     }
 
 
